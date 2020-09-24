7b84757f40f35083205f31bdbce92036a09976dc
ayubero <32571894+ayubero@users.noreply.github.com>
2020-09-24 12:53:49 +0200

    Add files via upload

--- suma_gauss_pares.py
+++ suma_gauss_pares.py
@@ -1,6 +1,6 @@
 def suma_gauss_pares(num):
     suma_acumulada = 0
-    for i in range(1, num+2):
+    for i in range(1, num+1):
         resto = i%2
         if resto == 0:
             suma_acumulada = suma_acumulada + i
