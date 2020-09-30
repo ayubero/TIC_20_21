42f34e99daaef2a741f206416d812e73bdec859d
ayubero <32571894+ayubero@users.noreply.github.com>
2020-09-30 09:46:00 +0200

    Add files via upload

---
+++ 1_multiplicador.py
@@ -0,0 +1,4 @@
+def multiplicar(a,b,c,d):
+    return a*b*c*d
+
+print multiplicar(2,3,4,5)
---
+++ 2_conversor_eur_dol.py
@@ -0,0 +1,5 @@
+# -*- coding: cp1252 -*-
+def convertir_dol_a_eur(dolares):
+    return dolares*0.85
+
+print str(convertir_dol_a_eur(50)) + " €"
