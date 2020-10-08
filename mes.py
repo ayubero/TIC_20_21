2b4cad7fbbcd4e0ca99b6de1564e0d58576df3a7
ayubero <32571894+ayubero@users.noreply.github.com>
2020-10-08 12:35:39 +0200

    Add files via upload

---
+++ mes.py
@@ -0,0 +1,25 @@
+# -*- coding: cp1252 -*-
+'''Pasar el número de un mes entre 1 y 12 a la función
+y esta nos devolverá el nombre del mes'''
+def mes(num):
+    lista_meses = [
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"
+    ]
+    if num > 0 and num < 13:
+        return lista_meses[num - 1]
+    else:
+        return "Tiene que introducir un número entre 1 y 12"
+
+numero_mes = input("Introduce el número de un mes: ")
+print mes(numero_mes)
