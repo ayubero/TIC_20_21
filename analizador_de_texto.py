2d9b89e507f937fb60a7843801d1505213d0d8b5
ayubero <32571894+ayubero@users.noreply.github.com>
2020-10-13 19:53:28 +0200

    Add files via upload

--- analizador_de_texto.py
+++ analizador_de_texto.py
@@ -1,14 +1,19 @@
 # -*- coding: cp1252 -*-
 def analizar_texto(texto):
     vocales = ["a","e","i","o","u"]
+    consonantes = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
     num_vocales = 0
+    num_consonantes = 0
 
     for item in texto:
         for v in vocales:
             if item == v:
                 num_vocales += 1
         for c in consonantes:
+            if item == c:
+                num_consonantes += 1
+    return "Hay "+str(num_vocales)+" vocales y "+str(num_consonantes)+" consonantes"
             
 
 #No introducir mayúsculas, tildes o ñ
-analixar_texto("esto es una prueba")
+print(analizar_texto("esto es una prueba"))
