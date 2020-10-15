dd77ce1df072d948d6cacf5eaf68c9e1108027a8
ayubero <32571894+ayubero@users.noreply.github.com>
2020-10-15 12:29:00 +0200

    Add files via upload

---
+++ juego_adivina_el_numero.py
@@ -0,0 +1,21 @@
+# -*- coding: cp1252 -*-
+import random
+
+def adivinar():
+    numero = random.randint(1,10)
+    print numero
+    intentos = 0
+    while True:
+        intentos += 1
+        intento = input("Adivina el número, está entre el 1 al 10, ambos incluidos: ")
+        if intento == numero:
+            print "Enhorabuena. Has adivinado el número en el intento número "+str(intentos)
+            break
+        elif intento < numero:
+            print "El número que has de adivinar es más grande"
+        elif intento > numero:
+            print "El número que has de adivinar es más pequeño"
+        else:
+            "Error"
+
+adivinar()
