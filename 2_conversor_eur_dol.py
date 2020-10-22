81ae7b1223b41d075dbf5a84b567e4d804ac165b
ayubero <32571894+ayubero@users.noreply.github.com>
2020-10-22 12:39:12 +0200

    Add files via upload

--- 2_conversor_eur_dol.py
+++ 2_conversor_eur_dol.py
@@ -1,5 +1,9 @@
 # -*- coding: cp1252 -*-
-def convertir_dol_a_eur(dolares):
-    return dolares*0.85
+def convertir_dol_a_eur():
+    dolares = input("Introduce el número de dólares que quieres convertir a euros: ")
+    if type(dolares) == int or type(dolares) == float:
+        print str(dolares*0.85) + " €"
+    else:
+        print "No has introducido un número válido"
 
-print str(convertir_dol_a_eur(50)) + " €"
+convertir_dol_a_eur()
