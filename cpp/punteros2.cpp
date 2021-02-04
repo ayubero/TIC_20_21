1601037f6b8c42957a0d99fafe65703a71ba822b
ayubero <32571894+ayubero@users.noreply.github.com>
2021-02-04 12:37:37 +0100

    Update punteros2.cpp

--- cpp/punteros2.cpp
+++ cpp/punteros2.cpp
@@ -7,8 +7,8 @@ int main() {
 	char aux[20];
 	int longitud;
 	
-	fot(int i = 0; i < 3; i++) {
-		printf("\npalabra %d", cont);
+	for(int i = 0; i < 3; i++) {
+		printf("\npalabra %d: ", i);
 		scanf("%s", aux);
 		longitud = strlen(aux);
 		lista[i] = (char *) malloc(longitud);
@@ -19,4 +19,7 @@ int main() {
 	printf("\n**********");
 	printf("\n  LISTA  ");
 	printf("\n**********");
+	for(int i = 0; i < 3; i++) {
+		printf("\n %d .- %s", i, *(lista+i));
+	}
 }
